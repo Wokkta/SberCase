fd77e01f2acd0436a9dd31e1b9f7b30dd2d5ac6d
Wokkta <awerty17@mail.ru>
2022-08-25 15:35:11 +0300

    a

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,22 +5,25 @@ import Try_Btn from "./Try_Btn";
 import { useState } from "react";
 export default function Welcome(props) {
   return (
-    <>{/*
+    <>
       <Header logged={props.logged} />
       <section className="section-one">
         <div className="slogan slogan-sec-one">
-          Создайте <span>подборку </span>мероприятий, секции и курсов <span>под интересы Вашего
-          ребенка</span>
+          Создайте <span>подборку </span>мероприятий, секции и курсов{" "}
+          <span>под интересы Вашего ребенка</span>
         </div>
         <Try_Btn />
         <div className="rocket-boy"></div>
       </section>
       <Benefits />
-       */}
+
       <section className="section-three">
         <Catalog />
+        <Try_Btn />
       </section>
-      
+      <section className="section-four">
+        
+      </section>
     </>
   );
 }

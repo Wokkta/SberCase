935a322c453e56c5ea808eb5e9256ca4d3763c9e
Wokkta <awerty17@mail.ru>
2022-08-25 15:26:56 +0300

    div

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -2,23 +2,23 @@ export default function Catalog(items) {
   return (
     <div className="catalog">
       <div className="catalog-category catalog-category-1">
-        <img src="#" alt="" className="catalog-category-img-1" />
+        <div className="catalog-category-img-1" />
         Образование
       </div>
       <div className="catalog-category catalog-category-2">
-        <img src="#" alt="" className="catalog-category-img-2" />
+        <div className="catalog-category-img-2" />
         творчество
       </div>
       <div className="catalog-category catalog-category-3">
-        <img src="#" alt="" className="catalog-category-img-3" />
+        <div className="catalog-category-img-3" />
         досуг
       </div>
       <div className="catalog-category catalog-category-4">
-        <img src="#" alt="" className="catalog-category-img-4" />
+        <div className="catalog-category-img-4" />
         спорт
       </div>
       <div className="catalog-category catalog-category-5">
-        <img src="#" alt="" className="catalog-category-img-5" />
+        <div className="catalog-category-img-5" />
         соц. активность
       </div>
     </div>
